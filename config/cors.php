b9599bce997e53435939f1611a36d6361759ce64
TryAgain1122 <luisrafhael1122@gmail.com>
2026-02-24 08:41:04 +0800

    update cors

--- config/cors.php
+++ config/cors.php
@@ -10,18 +10,16 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:3000'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => false,
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://www.afhome.ph',
+        'https://afhome.ph',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+],
+'allowed_origins_patterns' => [
+    '#^https://.*\.vercel\.app$#',
+],
+'supports_credentials' => true,
 
 ];
